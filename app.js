c5391b9aca0486e48a048aa4e5ce6093bf9e9687
MitreaDoru <mitrea.doru.catalin@gmail.com>
2023-11-10 15:32:24 +0200

    Second

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 const httpServer = require("http").createServer(app);
 const io = require('socket.io')(httpServer, {
     cors: {
-        origin: 'https://live-chat-scql.onrender.com/'
+        origin: 'https://mitreadoru.github.io/live-chat'
     }
 });
 
